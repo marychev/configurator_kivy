cec959c3a73e00928ff23c803f53587583485b64
Mihail <pyapmail@gmail.com>
2018-07-01 11:16:13 +0300

    123

--- app/node/node.py
+++ app/node/node.py
@@ -1,7 +1,7 @@
 import kivy
 kivy.require('1.11.0')
 from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import ObjectProperty
+from kivy.properties import ObjectProperty, StringProperty
 from netaddr import IPAddress
 import socket
 from app.fileconfig import FileConfig
@@ -16,6 +16,8 @@ class NodeWidget(BoxLayout):
     def __init__(self):
         super().__init__()
         self.set_active()
+
+    # id = StringProperty('id_node_widget')
 
     container_home = ObjectProperty()
     node_name_text_input = ObjectProperty()
